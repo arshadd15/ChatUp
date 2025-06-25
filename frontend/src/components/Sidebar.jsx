eee85e0ce30385017995cda89c7645ae2ad488c7
Arshad <arshaghnachi234@gmail.com>
2025-06-25 17:33:30 +0530

    Responsive UI changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
 
-const Sidebar = () => {
+const Sidebar = ({ isMobile = false }) => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } =
     useChatStore();
   const { onlineUsers } = useAuthStore();
@@ -21,11 +21,15 @@ const Sidebar = () => {
   if (isUsersLoading) return <SidebarSkeleton />;
 
   return (
-    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+    <aside
+      className={`h-full ${
+        isMobile ? "w-full" : "lg:w-72"
+      } border-r border-base-300 flex flex-col transition-all duration-200`}
+    >
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="size-6" />
-          <span className="font-medium hidden lg:block">Contacts</span>
+          <span className="font-medium block">Contacts</span>
         </div>
         <div className="mt-3 hidden lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
@@ -71,8 +75,7 @@ const Sidebar = () => {
               )}
             </div>
 
-            {/* User info - only visible on larger screens */}
-            <div className="hidden lg:block text-left min-w-0">
+            <div className="block text-left min-w-0">
               <div className="font-medium truncate">{user.fullName}</div>
               <div className="text-sm text-zinc-400">
                 {onlineUsers.includes(user._id) ? "Online" : "Offline"}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,13 +7,24 @@ import ChatContainer from "../components/ChatContainer";
 const HomePage = () => {
   const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
+    <div className="h-full lg:h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-15 lg:pt-20 lg:px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-6rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
+            {/* Mobile View: Show only Sidebar or ChatContainer */}
+            <div className="flex w-full lg:hidden">
+              {!selectedUser ? (
+                <Sidebar isMobile />
+              ) : (
+                <ChatContainer isMobile />
+              )}
+            </div>
 
-            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            {/* Desktop View */}
+            <div className="hidden lg:flex w-full">
+              <Sidebar />
+              {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            </div>
           </div>
         </div>
       </div>
