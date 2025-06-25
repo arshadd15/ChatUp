f2d70c5eefcc89718071c0eeb4c98e3a945e1eb8
Arshad <arshaghnachi234@gmail.com>
2025-06-25 15:45:31 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,6 @@ const messageLimiter = ratelimiter({
 });
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
@@ -40,12 +39,14 @@ app.use("/api/auth", authLimiter, authRoutes);
 app.use("/api/messages", messageLimiter, messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get(
     ("*",
     (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+      res.sendFile(
+        path.join(__dirname, "../../frontend", "dist", "index.html")
+      );
     })
   );
 }
