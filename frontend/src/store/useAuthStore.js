3d8b0beecbd2c3ce17898d1e455f87c0121437c4
Arshad <arshaghnachi234@gmail.com>
2025-06-26 18:48:23 +0530

    updated sign in  method for user friendliness

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -46,6 +46,7 @@ export const useAuthStore = create((set, get) => ({
   },
 
   login: async (data) => {
+    set({ isLoggingIn: true });
     try {
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
